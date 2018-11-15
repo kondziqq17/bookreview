534d5bae1f05d18c957f55040a8ee1c794064050
Konrad Pason <konrad.pason17@gmail.com>
2018-11-15 11:39:31 +0100

    Implemented navbar, added user_id to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_125801) do
+ActiveRecord::Schema.define(version: 2018_11_15_103750) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_125801) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
